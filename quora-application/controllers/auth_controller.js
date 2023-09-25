13c67a471558dcbf9d101248ebdee711eeae4c63
webdevsunil <webdevelopersunil>
2023-09-25 16:54:18 +0530

    Locals set for the login,register page

--- quora-application/controllers/auth_controller.js
+++ quora-application/controllers/auth_controller.js
@@ -1,10 +1,22 @@
 
 module.exports.index = (req, res) => {  // function for load the default home/login page
+    
+    const data = {
+        href : '/sign-up',
+        a_content : "Sign up with email",
+        title : "Register"
+    };
 
-    return res.render('auth/login');    // rendering the login page
+    return res.render('auth/login', data);    // rendering the login page
 };
 
 module.exports.sign_up = (req, res) => {    //  function for load the register/signup page
 
-    return res.render('auth/register');
+    const data = {
+        href : '/login',
+        a_content : "Log-in with email",
+        title : "Register"
+    };
+
+    return res.render('auth/register', data);
 }
